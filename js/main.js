779b685443a29eaad6e87c7877c87e4163e2d23c
Dulce francelis Perez castillo <dulceperez@Dulces-MacBook-Pro.local>
2020-05-04 23:07:33 -0400

    Adding template admin

--- js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $('.main-menu li').on('click',function(){
-        if(this) {
-           $(this).addClass("active");
-           $('.main-menu li').not($(this)).removeClass("active")
-        }
-    });
-});
